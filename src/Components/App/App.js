2e783ce2fca4ba6ce1acaf482ef4029b18ba5c61
Dai <873342140@qq.com>
2018-05-02 16:45:46 +0800

    test

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,15 +2,46 @@ import React, { Component } from 'react';
 import './App.css';
 import {NavLink} from "react-router-dom";
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      isShow:false
+    }
+  }
   render() {
     return (
       <div className="App">
+      	<header>
+      			   <i className="icon iconfont icon-liebiao1"></i>
+               <span>锤子科技商城</span>
+               <span isShow={this.state.isShow}>编辑</span>
+      	</header>
+        <content>
+                {this.props.children}
+        </content>
+         
         <footer>
-        		<li><NavLink to="/home" activeClassName="active">首页</NavLink></li>
-                <li><NavLink to="/classify" activeClassName="active">分类</NavLink></li>
-                <li><NavLink to="/car" activeClassName="active">购物车</NavLink></li>
+        		<span>
+                <NavLink to="/home" activeClassName="active" className="btn">
+                 <i class="icon iconfont icon-zhuye"></i>
+                    首页
+                </NavLink>
+
+            </span>
+            <span>
+                <NavLink to="/classify" activeClassName="active" className="btn">
+                  <i class="icon iconfont icon-liebiao"></i>
+                    分类
+                </NavLink>
+            </span>
+            <span>
+                <NavLink to="/car" activeClassName="active" className="btn">
+                <i class="icon iconfont icon-56"></i>
+                购物车
+            </NavLink>
+            </span>
         </footer>
-        {this.props.children}
+       
       </div>
     );
   }
