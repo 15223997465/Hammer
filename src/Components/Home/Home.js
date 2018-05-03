54b22b9f03eeb4d369b278c607ed015a3f5ca9ea
TH <757901093@qq.com>
2018-05-03 17:45:23 +0800

    home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Home.css';
-
+import ReactSwipe from 'react-swipe';
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      banner: [],
+      shortcut:[],
+    }
+  }
   render() {
-    return (
-      <div>
-      home
+    return (<div id="Home">
+      <div className="banner">
+        <ReactSwipe className="carousel loopbanner" swipeOptions={{
+            continuous: true,
+            auto: 3000
+          }} key={this.state.banner.length}>
+          {
+            this.state.banner.map(item => {
+              return <a href={item.linkUrl} key={item.beginTime}><img src={item.src} alt="bannerimg"/></a>
+            })
+          }
+        </ReactSwipe>
+        <ul className="bannerlist">
+          {
+            this.state.shortcut.map(item=>{
+              return <li key={item.src}><a href={item.linkUrl}><img src={item.src} alt="bannerlistimg"/><p>{item.labelTitle}</p></a></li>
+            })
+          }
+        </ul>
       </div>
-    );
+    </div>);
+  }
+  componentDidMount() {
+    fetch('../../../home.json').then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          this.setState({
+            banner: data.banner.dataList,
+            shortcut:data.shortcut.dataList,
+          })
+          console.log(this.state.shortcut)
+        })
+      }
+    }).catch((res) => {
+      console.log(res.status);
+    });
+
   }
 }
 
