a6b62b705bc6bd003579cfff46e1e3b5dadfe681
xiongxin1996 <199705161@qq.com>
2018-05-04 14:40:20 +0800

    5-4

--- src/Components/Classify/Classify.js
+++ src/Components/Classify/Classify.js
@@ -7,35 +7,42 @@ class Classify extends Component {
 		this.state={
 			datalist:null,
 			alldata:null,
-			detaiId:null
+			tempList:[],
+			temp:[],
+			data:[]
 		}
 	}
   render() {
+  	const arr=["11","22"]
+
     return (
       <div className="classify">
-  {
-  		this.state.datalist?
-  		<div>
-  			
-  			{/*<h2>{this.state.datalist[0].layout.dataList[0].sku}</h2>
-  			  			<h2>{this.state.datalist[0].layout.dataList[1].sku}</h2>
-  			  			<h2>{this.state.datalist[0].layout.dataList[2].sku}</h2>
-						<img src={item.poster.origin}/>
-  			  		*/}
-	  			{
-	  				this.state.datalist.map(item=>
-	  					<div className="liststyle" >
-	  						<h2>{item.name}</h2>
-	  						<img src={item.image.src}/>
-	  					</div>
-	  				
+      {
+      	this.state.data&&this.state.alldata?
+      		<div className="container">
+        			{
+        				this.state.data.map(item=>
+        					<div className="outbox" key={item.title}>
+	        					<p className="title">{item.title}</p>
+	        					<p className="logo"><img src={item.src} /></p>
+	        					{console.log(item.length)}
+	        				<ul className="one">
+	        				
+	        			{item.map(item=>	        				
+	        				<li key={item.id} onClick={this.handleClick.bind(this,item.id)}>
+	        				<img src={item.shop_info.ali_image}/>
+	        				<p>{item.shop_info.sku_mobile_title}</p>
+	        				<i className="iconfont icon-iconfontjiantou5"></i>
+	        				</li>
+	        			)}
+	        			</ul>
+        					</div>
+    			      )
+        			}
+        	</div>
+        				:null
 
-	  				)
-	  			}
-
-		</div>
-  		:null
-  	}
+      }
   
       </div>
   
@@ -43,34 +50,74 @@ class Classify extends Component {
   
     );
   }
-  componentDidMount(){
+  handleClick(id){
+		
+		this.props.history.push(`/detail/${id}`); 
+		console.log(id);
+	}
+  componentWillMount(){
 		axios.get("/marketing/mobile/category_3de2b51f6fd72a45304bee018af1a54a.json").then(res=>{
 			this.setState({
 				datalist:res.data,
 			})
-			console.log(this.state.datalist[0]);
-			console.log(res.data[0].layout.dataList[0].sku)
-			this.state.datalist.map(item=>
-				this.setState({
-				alldata:item.layout
+			console.log(this.state.datalist);
+
+
+
+
+//*******************************************************************************
+			axios.get("/product/skus?ids=100040602,100035101,100027305,100036303,100035702,100036401,100036501,100041902,100042101,100027101,100035203,100026601,100026701,100026801,100022501,100029902,100022803,100042801,100037801,100035801,100027001,100034901,100023001,100039709,100040132,100045615,100044009,100040915,100041001,100025103,100032901,100045301,100042301,100040401,100041201,100032701,100032601,100032201,100022201,100022901,100032701,100032601&with_stock=true&with_spu=true").then(res=>{
+			this.setState({
+				alldata:res.data.data.list
+			})
+			console.log(this.state.alldata);
+
+
+			//******************
+	for(var m=0;m<this.state.datalist.length;m++)
+	{
+			for(var i=0;i<this.state.datalist[m].layout.dataList.length;i++)
+			{
+				this.state.temp.push(this.state.datalist[m].layout.dataList[i].sku);
 				
-			}),
-			// this.state.alldata.map(item=>
-			// 	this.setState({
-			// 	detaiId:item.layout.dataList.sku
-				
-			// })
-			// 	)
-			)
-			console.log(this.state.alldata)
-			console.log(this.state.detaiId)
+			}
+			for(var i=0;i<this.state.temp.length;i++)
+			{
+				for(var j=0;j<this.state.alldata.length;j++)
+				{
+					if(this.state.temp[i]==this.state.alldata[j].id)
+					{	
+						this.state.tempList.push(this.state.alldata[j]);
+
+					}
+
+				}
+			}
+			this.state.data.push(this.state.tempList);
+			this.state.temp=[];
+			//console.log(this.state.tempList.title=this.state.datalist[m].name)
+			this.state.tempList.title=this.state.datalist[m].name;	
+			this.state.tempList.src=this.state.datalist[m].image.src;	
+			this.state.tempList=[];
+	}		
+	this.setState({
+				data:this.state.data
+			})		
+		console.log(this.state.data);	
+
 		})
-		// axios.get("/product/skus?ids=100040602,100035101,100027305,100036303,100035702,100036401,100036501,100041902,100042101,100027101,100035203,100026601,100026701,100026801,100022501,100029902,100022803,100042801,100037801,100035801,100027001,100034901,100023001,100039709,100040915,100041001,100026113,100037719,100026445,100025103,100032901,100045301,100042301,100040401,100041201,100032701,100032601,100032201,100022201,100022901,100032701,100032601&with_stock=true&with_spu=true").then(res=>{
-		// 	this.setState({
-				
-		// 	})
-		// 	console.log(res.data);
-		// })
+
+//*******************************************************************************
+
+
+
+
+
+
+		})
+
+		
+
 	}
 }
 
