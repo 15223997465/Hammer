e2307410666d1520c5d928ff74187ce96251e3a8
Dai <873342140@qq.com>
2018-05-04 19:54:51 +0800

    是的

--- src/Components/Detail/Detail.js
+++ src/Components/Detail/Detail.js
@@ -56,18 +56,6 @@ class Detail extends Component {
           
       </div>
       )
-    
-=======
-      <div className="detailBox">
-      		<ul className="detailheader">
-      			<li><a href="#">商品</a></li>
-      			<li><a href="#">详情</a></li>
-      			<li><a href="#">参数</a></li>
-      			<li><a href="#">推荐</a></li>
-      		</ul>
-      </div>
-    );
->>>>>>> .merge_file_a12744
   }
 
   componentDidMount(){
